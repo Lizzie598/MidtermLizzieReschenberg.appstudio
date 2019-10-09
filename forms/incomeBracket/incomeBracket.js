f33114020c44e50a85b0edb2c4e208dbb3eb9911
Lizzie <ekr74869@creighton.edu>
2019-10-09 16:43:43 -0500

    j

--- forms/incomeBracket/incomeBracket.js
+++ forms/incomeBracket/incomeBracket.js
@@ -1,17 +1,21 @@
 
 function taxBracket(I) {
-  return 
+  return lblReturn.value
+  }
 
 btnSubmit.onclick=function(){
-  lblReturn.value = With your income of ${inptIncome}, you are in a tax bracket of ${taxBracket}
+  if (inptIncome.value < 30000) {
+    lblReturn.value = With your income of ${inptIncome.value}, you are in a tax bracket of 8%
+  } else if (inptIncome.value >= 30000 && < 99999) {
+    lblReturn.value = With your income of ${inptIncome.value}, you are in a tax bracket of 15%
+  } else if (inptIncome.value >= 99999) {
+    lblReturn.value = With your income of ${inptIncome.value}, you are in a tax bracket of 25%
+  } else {
+    lblReturn.value = no entry
+  }
 }
 
-if (inptIncome.value < 30000) {
-  lblReturn.value = With your income of ${inptIncome}, you are in a tax bracket of 8%
-} else if (inptIncome.value >= 30000 && < 99999) {
-  lblReturn.value = With your income of ${inptIncome}, you are in a tax bracket of 15%
-} else if (inptIncome.value >= 99999) {
-  lblReturn.value = With your income of ${inptIncome}, you are in a tax bracket of 25%
-} else {
-  lblReturn.value = no entry
+
+btnGoAgain.onclick=function(){
+  inputIncome.value == ""
 }
