e4bd1f3726ca875c88951bfa86f917fd9a6981df
Lizzie <ekr74869@creighton.edu>
2019-10-09 15:49:34 -0500

    s

--- forms/animals/animals.js
+++ forms/animals/animals.js
@@ -1,1 +1,6 @@
 let animals = ["dog", "cat", "horse", "mouse", "pig", "cow", "ferret", "lizard", "frog"]
+
+let newAnimal = prompt("What is your favorite animal's name?")
+let newAnimals = animals.push(newAnimal.toLowerCase)
+
+console.log(`The last animal is ${newAnimals}`)
